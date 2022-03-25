1fc833c5b01547ffeae5dae16e77277196e73e0e
= <=>
2022-03-25 10:38:11 -0400

    production path updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,47 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/Game.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: [path.resolve(__dirname, 'src')]
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        filename: 'gamebundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: "src/css", to: "css" },
-                { from: "index.html", to: "." },
-                { from: "images", to: "images" }
-            ],
-        }),
-    ],
-    devServer: {
-        liveReload: true,
-        hot: false,
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-            publicPath: '/dist',
-            watch: true,
-        }
-    }
+module.exports = (env, argv) => {
+    const isDev = argv !== undefined && "mode" in argv ? argv.mode === "development" : true;
+
+    var config = {
+        mode: isDev ? 'development' : 'production',
+        entry: './src/Game.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    include: [path.resolve(__dirname, 'src')]
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        output: {
+            filename: 'gamebundle.js',
+            path: isDev ? path.resolve(__dirname, 'dist') : '.'
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: "src/css", to: "css" },
+                    { from: "index.html", to: "." },
+                    { from: "images", to: "images" }
+                ],
+            }),
+        ],
+        devServer: isDev ? {
+            liveReload: true,
+            hot: false,
+            static: {
+                directory: path.resolve(__dirname, 'dist'),
+                publicPath: '/dist',
+                watch: true,
+            }
+        } : {}
+    };
+
+    return [config];
 }
